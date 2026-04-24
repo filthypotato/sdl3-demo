88246e25943b8ee903bc5a690d2def11a07ac08a
filthypotato <tylorromine94@gmail.com>
2026-04-23 21:41:12 -0700

    Fixed extra nest folder of scripts

---
+++ build_linux/CMakeFiles/sdl3-demo.dir/link.d
@@ -0,0 +1,106 @@
+sdl3-demo: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtbeginS.o \
+  CMakeFiles/sdl3-demo.dir/sdl3-demo/sdl3-demo.cpp.o \
+  /usr/local/lib/libSDL3_image.so.0.3.2 \
+  /usr/local/lib/libSDL3_mixer.so.0.1.0 \
+  /usr/local/lib/libglm.a \
+  /usr/local/lib/libSDL3.so.0.5.0 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libstdc++.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crtn.o \
+  /usr/lib/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtbeginS.o:
+
+CMakeFiles/sdl3-demo.dir/sdl3-demo/sdl3-demo.cpp.o:
+
+/usr/local/lib/libSDL3_image.so.0.3.2:
+
+/usr/local/lib/libSDL3_mixer.so.0.1.0:
+
+/usr/local/lib/libglm.a:
+
+/usr/local/lib/libSDL3.so.0.5.0:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libstdc++.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crtn.o:
+
+/usr/lib/ld-linux-x86-64.so.2:
---
+++ build_linux/CMakeFiles/sdl3-demo.dir/sdl3-demo/sdl3-demo.cpp.o.d
@@ -0,0 +1,345 @@
+CMakeFiles/sdl3-demo.dir/sdl3-demo/sdl3-demo.cpp.o: \
+ /home/filthypotato/Documents/Projects/sdl3-demo/sdl3-demo/sdl3-demo.cpp \
+ /usr/include/stdc-predef.h /usr/local/include/SDL3/SDL.h \
+ /usr/local/include/SDL3/SDL_stdinc.h \
+ /usr/local/include/SDL3/SDL_platform_defines.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h \
+ /usr/include/string.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stddef.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/include/wchar.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h /usr/include/bits/wchar.h \
+ /usr/include/bits/types/wint_t.h /usr/include/bits/types/mbstate_t.h \
+ /usr/include/bits/types/__mbstate_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h \
+ /usr/local/include/SDL3/SDL_begin_code.h \
+ /usr/local/include/SDL3/SDL_close_code.h \
+ /usr/local/include/SDL3/SDL_assert.h \
+ /usr/local/include/SDL3/SDL_asyncio.h \
+ /usr/local/include/SDL3/SDL_atomic.h /usr/local/include/SDL3/SDL_audio.h \
+ /usr/local/include/SDL3/SDL_endian.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/endianness.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/uintn-identity.h \
+ /usr/local/include/SDL3/SDL_error.h /usr/local/include/SDL3/SDL_mutex.h \
+ /usr/local/include/SDL3/SDL_thread.h \
+ /usr/local/include/SDL3/SDL_properties.h \
+ /usr/local/include/SDL3/SDL_iostream.h \
+ /usr/local/include/SDL3/SDL_bits.h \
+ /usr/local/include/SDL3/SDL_blendmode.h \
+ /usr/local/include/SDL3/SDL_camera.h \
+ /usr/local/include/SDL3/SDL_pixels.h \
+ /usr/local/include/SDL3/SDL_surface.h /usr/local/include/SDL3/SDL_rect.h \
+ /usr/local/include/SDL3/SDL_clipboard.h \
+ /usr/local/include/SDL3/SDL_cpuinfo.h \
+ /usr/local/include/SDL3/SDL_dialog.h /usr/local/include/SDL3/SDL_video.h \
+ /usr/local/include/SDL3/SDL_dlopennote.h \
+ /usr/local/include/SDL3/SDL_events.h \
+ /usr/local/include/SDL3/SDL_gamepad.h /usr/local/include/SDL3/SDL_guid.h \
+ /usr/local/include/SDL3/SDL_joystick.h \
+ /usr/local/include/SDL3/SDL_power.h /usr/local/include/SDL3/SDL_sensor.h \
+ /usr/local/include/SDL3/SDL_keyboard.h \
+ /usr/local/include/SDL3/SDL_keycode.h \
+ /usr/local/include/SDL3/SDL_scancode.h \
+ /usr/local/include/SDL3/SDL_mouse.h /usr/local/include/SDL3/SDL_pen.h \
+ /usr/local/include/SDL3/SDL_touch.h \
+ /usr/local/include/SDL3/SDL_filesystem.h \
+ /usr/local/include/SDL3/SDL_gpu.h /usr/local/include/SDL3/SDL_haptic.h \
+ /usr/local/include/SDL3/SDL_hidapi.h /usr/local/include/SDL3/SDL_hints.h \
+ /usr/local/include/SDL3/SDL_init.h /usr/local/include/SDL3/SDL_loadso.h \
+ /usr/local/include/SDL3/SDL_locale.h /usr/local/include/SDL3/SDL_log.h \
+ /usr/local/include/SDL3/SDL_messagebox.h \
+ /usr/local/include/SDL3/SDL_metal.h /usr/local/include/SDL3/SDL_misc.h \
+ /usr/local/include/SDL3/SDL_platform.h \
+ /usr/local/include/SDL3/SDL_process.h \
+ /usr/local/include/SDL3/SDL_render.h \
+ /usr/local/include/SDL3/SDL_storage.h \
+ /usr/local/include/SDL3/SDL_system.h /usr/local/include/SDL3/SDL_time.h \
+ /usr/local/include/SDL3/SDL_timer.h /usr/local/include/SDL3/SDL_tray.h \
+ /usr/local/include/SDL3/SDL_version.h \
+ /usr/local/include/SDL3/SDL_oldnames.h \
+ /usr/local/include/SDL3/SDL_main.h \
+ /usr/local/include/SDL3_image/SDL_image.h \
+ /usr/local/include/SDL3_mixer/SDL_mixer.h /usr/include/c++/15.2.1/vector \
+ /usr/include/c++/15.2.1/bits/requires_hosted.h \
+ /usr/include/c++/15.2.1/x86_64-pc-linux-gnu/bits/c++config.h \
+ /usr/include/c++/15.2.1/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /usr/include/c++/15.2.1/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /usr/include/c++/15.2.1/pstl/pstl_config.h \
+ /usr/include/c++/15.2.1/bits/stl_algobase.h \
+ /usr/include/c++/15.2.1/bits/functexcept.h \
+ /usr/include/c++/15.2.1/bits/exception_defines.h \
+ /usr/include/c++/15.2.1/bits/cpp_type_traits.h \
+ /usr/include/c++/15.2.1/bits/version.h \
+ /usr/include/c++/15.2.1/type_traits \
+ /usr/include/c++/15.2.1/ext/type_traits.h \
+ /usr/include/c++/15.2.1/ext/numeric_traits.h \
+ /usr/include/c++/15.2.1/bits/stl_pair.h \
+ /usr/include/c++/15.2.1/bits/move.h \
+ /usr/include/c++/15.2.1/bits/utility.h /usr/include/c++/15.2.1/compare \
+ /usr/include/c++/15.2.1/concepts \
+ /usr/include/c++/15.2.1/bits/stl_iterator_base_types.h \
+ /usr/include/c++/15.2.1/bits/iterator_concepts.h \
+ /usr/include/c++/15.2.1/bits/ptr_traits.h \
+ /usr/include/c++/15.2.1/bits/ranges_cmp.h \
+ /usr/include/c++/15.2.1/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/15.2.1/bits/concept_check.h \
+ /usr/include/c++/15.2.1/debug/assertions.h \
+ /usr/include/c++/15.2.1/bits/stl_iterator.h /usr/include/c++/15.2.1/new \
+ /usr/include/c++/15.2.1/bits/exception.h \
+ /usr/include/c++/15.2.1/bits/stl_construct.h \
+ /usr/include/c++/15.2.1/debug/debug.h \
+ /usr/include/c++/15.2.1/bits/predefined_ops.h \
+ /usr/include/c++/15.2.1/bit /usr/include/c++/15.2.1/bits/allocator.h \
+ /usr/include/c++/15.2.1/x86_64-pc-linux-gnu/bits/c++allocator.h \
+ /usr/include/c++/15.2.1/bits/new_allocator.h \
+ /usr/include/c++/15.2.1/bits/memoryfwd.h \
+ /usr/include/c++/15.2.1/bits/stl_uninitialized.h \
+ /usr/include/c++/15.2.1/ext/alloc_traits.h \
+ /usr/include/c++/15.2.1/bits/alloc_traits.h \
+ /usr/include/c++/15.2.1/bits/stl_vector.h \
+ /usr/include/c++/15.2.1/initializer_list \
+ /usr/include/c++/15.2.1/bits/ranges_base.h \
+ /usr/include/c++/15.2.1/bits/max_size_type.h \
+ /usr/include/c++/15.2.1/numbers \
+ /usr/include/c++/15.2.1/bits/stl_bvector.h \
+ /usr/include/c++/15.2.1/bits/functional_hash.h \
+ /usr/include/c++/15.2.1/bits/hash_bytes.h \
+ /usr/include/c++/15.2.1/bits/refwrap.h \
+ /usr/include/c++/15.2.1/bits/invoke.h \
+ /usr/include/c++/15.2.1/bits/stl_function.h \
+ /usr/include/c++/15.2.1/backward/binders.h \
+ /usr/include/c++/15.2.1/bits/range_access.h \
+ /usr/include/c++/15.2.1/bits/vector.tcc \
+ /usr/include/c++/15.2.1/bits/memory_resource.h \
+ /usr/include/c++/15.2.1/cstddef \
+ /usr/include/c++/15.2.1/bits/uses_allocator.h \
+ /usr/include/c++/15.2.1/bits/uses_allocator_args.h \
+ /usr/include/c++/15.2.1/tuple /usr/include/c++/15.2.1/bits/ranges_util.h \
+ /usr/include/c++/15.2.1/string /usr/include/c++/15.2.1/bits/stringfwd.h \
+ /usr/include/c++/15.2.1/bits/char_traits.h \
+ /usr/include/c++/15.2.1/bits/postypes.h /usr/include/c++/15.2.1/cwchar \
+ /usr/include/c++/15.2.1/bits/localefwd.h \
+ /usr/include/c++/15.2.1/x86_64-pc-linux-gnu/bits/c++locale.h \
+ /usr/include/c++/15.2.1/clocale /usr/include/locale.h \
+ /usr/include/bits/locale.h /usr/include/c++/15.2.1/iosfwd \
+ /usr/include/c++/15.2.1/cctype /usr/include/ctype.h \
+ /usr/include/c++/15.2.1/bits/ostream_insert.h \
+ /usr/include/c++/15.2.1/bits/cxxabi_forced.h \
+ /usr/include/c++/15.2.1/bits/basic_string.h \
+ /usr/include/c++/15.2.1/string_view \
+ /usr/include/c++/15.2.1/bits/string_view.tcc \
+ /usr/include/c++/15.2.1/ext/string_conversions.h \
+ /usr/include/c++/15.2.1/cstdlib /usr/include/stdlib.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/sys/types.h /usr/include/bits/types/clock_t.h \
+ /usr/include/bits/types/clockid_t.h /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/timer_t.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h \
+ /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-bsearch.h /usr/include/bits/stdlib-float.h \
+ /usr/include/c++/15.2.1/bits/std_abs.h /usr/include/c++/15.2.1/cstdio \
+ /usr/include/stdio.h /usr/include/bits/types/__fpos_t.h \
+ /usr/include/bits/types/__fpos64_t.h \
+ /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/stdio.h \
+ /usr/include/c++/15.2.1/cerrno /usr/include/errno.h \
+ /usr/include/bits/errno.h /usr/include/linux/errno.h \
+ /usr/include/asm/errno.h /usr/include/asm-generic/errno.h \
+ /usr/include/asm-generic/errno-base.h /usr/include/bits/types/error_t.h \
+ /usr/include/c++/15.2.1/bits/charconv.h \
+ /usr/include/c++/15.2.1/bits/basic_string.tcc \
+ /usr/include/c++/15.2.1/array /usr/include/unistd.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/confname.h /usr/include/bits/getopt_posix.h \
+ /usr/include/bits/getopt_core.h /usr/include/bits/unistd_ext.h \
+ /usr/include/linux/close_range.h \
+ /home/filthypotato/Documents/Projects/sdl3-demo/sdl3-demo/gameobject.h \
+ /usr/local/include/glm/glm.hpp /usr/local/include/glm/detail/_fixes.hpp \
+ /usr/include/c++/15.2.1/cmath /usr/include/math.h \
+ /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h /usr/include/bits/mathcalls-macros.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathcalls-narrow.h \
+ /usr/include/bits/iscanonical.h /usr/include/c++/15.2.1/bits/specfun.h \
+ /usr/include/c++/15.2.1/limits /usr/include/c++/15.2.1/tr1/gamma.tcc \
+ /usr/include/c++/15.2.1/tr1/special_function_util.h \
+ /usr/include/c++/15.2.1/tr1/bessel_function.tcc \
+ /usr/include/c++/15.2.1/tr1/beta_function.tcc \
+ /usr/include/c++/15.2.1/tr1/ell_integral.tcc \
+ /usr/include/c++/15.2.1/tr1/exp_integral.tcc \
+ /usr/include/c++/15.2.1/tr1/hypergeometric.tcc \
+ /usr/include/c++/15.2.1/tr1/legendre_function.tcc \
+ /usr/include/c++/15.2.1/tr1/modified_bessel_func.tcc \
+ /usr/include/c++/15.2.1/tr1/poly_hermite.tcc \
+ /usr/include/c++/15.2.1/tr1/poly_laguerre.tcc \
+ /usr/include/c++/15.2.1/tr1/riemann_zeta.tcc \
+ /usr/local/include/glm/detail/setup.hpp /usr/include/c++/15.2.1/cassert \
+ /usr/include/assert.h /usr/local/include/glm/simd/platform.h \
+ /usr/include/c++/15.2.1/cstdint /usr/include/c++/15.2.1/climits \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/limits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/pthread_stack_min-dynamic.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /usr/include/bits/uio_lim.h /usr/include/c++/15.2.1/cfloat \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/float.h \
+ /usr/local/include/glm/fwd.hpp \
+ /usr/local/include/glm/detail/qualifier.hpp \
+ /usr/local/include/glm/detail/setup.hpp /usr/local/include/glm/vec2.hpp \
+ /usr/local/include/glm/ext/vector_bool2.hpp \
+ /usr/local/include/glm/detail/type_vec2.hpp \
+ /usr/local/include/glm/detail/type_vec2.inl \
+ /usr/local/include/glm/detail/compute_vector_relational.hpp \
+ /usr/local/include/glm/ext/vector_bool2_precision.hpp \
+ /usr/local/include/glm/ext/vector_float2.hpp \
+ /usr/local/include/glm/ext/vector_float2_precision.hpp \
+ /usr/local/include/glm/ext/vector_double2.hpp \
+ /usr/local/include/glm/ext/vector_double2_precision.hpp \
+ /usr/local/include/glm/ext/vector_int2.hpp \
+ /usr/local/include/glm/ext/vector_int2_sized.hpp \
+ /usr/local/include/glm/ext/scalar_int_sized.hpp \
+ /usr/local/include/glm/detail/setup.hpp \
+ /usr/local/include/glm/ext/vector_uint2.hpp \
+ /usr/local/include/glm/ext/vector_uint2_sized.hpp \
+ /usr/local/include/glm/ext/scalar_uint_sized.hpp \
+ /usr/local/include/glm/vec3.hpp \
+ /usr/local/include/glm/ext/vector_bool3.hpp \
+ /usr/local/include/glm/detail/type_vec3.hpp \
+ /usr/local/include/glm/detail/type_vec3.inl \
+ /usr/local/include/glm/detail/compute_vector_decl.hpp \
+ /usr/include/c++/15.2.1/functional \
+ /usr/include/c++/15.2.1/bits/std_function.h \
+ /usr/include/c++/15.2.1/typeinfo /usr/include/c++/15.2.1/unordered_map \
+ /usr/include/c++/15.2.1/bits/unordered_map.h \
+ /usr/include/c++/15.2.1/bits/hashtable.h \
+ /usr/include/c++/15.2.1/bits/hashtable_policy.h \
+ /usr/include/c++/15.2.1/ext/aligned_buffer.h \
+ /usr/include/c++/15.2.1/bits/enable_special_members.h \
+ /usr/include/c++/15.2.1/bits/node_handle.h \
+ /usr/include/c++/15.2.1/bits/erase_if.h \
+ /usr/local/include/glm/detail/_vectorize.hpp \
+ /usr/local/include/glm/ext/vector_bool3_precision.hpp \
+ /usr/local/include/glm/ext/vector_float3.hpp \
+ /usr/local/include/glm/ext/vector_float3_precision.hpp \
+ /usr/local/include/glm/ext/vector_double3.hpp \
+ /usr/local/include/glm/ext/vector_double3_precision.hpp \
+ /usr/local/include/glm/ext/vector_int3.hpp \
+ /usr/local/include/glm/ext/vector_int3_sized.hpp \
+ /usr/local/include/glm/ext/vector_uint3.hpp \
+ /usr/local/include/glm/ext/vector_uint3_sized.hpp \
+ /usr/local/include/glm/vec4.hpp \
+ /usr/local/include/glm/ext/vector_bool4.hpp \
+ /usr/local/include/glm/detail/type_vec4.hpp \
+ /usr/local/include/glm/detail/type_vec4.inl \
+ /usr/local/include/glm/ext/vector_bool4_precision.hpp \
+ /usr/local/include/glm/ext/vector_float4.hpp \
+ /usr/local/include/glm/ext/vector_float4_precision.hpp \
+ /usr/local/include/glm/ext/vector_double4.hpp \
+ /usr/local/include/glm/ext/vector_double4_precision.hpp \
+ /usr/local/include/glm/ext/vector_int4.hpp \
+ /usr/local/include/glm/ext/vector_int4_sized.hpp \
+ /usr/local/include/glm/ext/vector_uint4.hpp \
+ /usr/local/include/glm/ext/vector_uint4_sized.hpp \
+ /usr/local/include/glm/mat2x2.hpp \
+ /usr/local/include/glm/ext/matrix_double2x2.hpp \
+ /usr/local/include/glm/detail/type_mat2x2.hpp \
+ /usr/local/include/glm/detail/type_mat2x2.inl \
+ /usr/local/include/glm/matrix.hpp /usr/local/include/glm/mat2x3.hpp \
+ /usr/local/include/glm/ext/matrix_double2x3.hpp \
+ /usr/local/include/glm/detail/type_mat2x3.hpp \
+ /usr/local/include/glm/detail/type_mat2x3.inl \
+ /usr/local/include/glm/ext/matrix_double2x3_precision.hpp \
+ /usr/local/include/glm/ext/matrix_float2x3.hpp \
+ /usr/local/include/glm/ext/matrix_float2x3_precision.hpp \
+ /usr/local/include/glm/mat2x4.hpp \
+ /usr/local/include/glm/ext/matrix_double2x4.hpp \
+ /usr/local/include/glm/detail/type_mat2x4.hpp \
+ /usr/local/include/glm/detail/type_mat2x4.inl \
+ /usr/local/include/glm/ext/matrix_double2x4_precision.hpp \
+ /usr/local/include/glm/ext/matrix_float2x4.hpp \
+ /usr/local/include/glm/ext/matrix_float2x4_precision.hpp \
+ /usr/local/include/glm/mat3x2.hpp \
+ /usr/local/include/glm/ext/matrix_double3x2.hpp \
+ /usr/local/include/glm/detail/type_mat3x2.hpp \
+ /usr/local/include/glm/detail/type_mat3x2.inl \
+ /usr/local/include/glm/ext/matrix_double3x2_precision.hpp \
+ /usr/local/include/glm/ext/matrix_float3x2.hpp \
+ /usr/local/include/glm/ext/matrix_float3x2_precision.hpp \
+ /usr/local/include/glm/mat3x3.hpp \
+ /usr/local/include/glm/ext/matrix_double3x3.hpp \
+ /usr/local/include/glm/detail/type_mat3x3.hpp \
+ /usr/local/include/glm/detail/type_mat3x3.inl \
+ /usr/local/include/glm/common.hpp \
+ /usr/local/include/glm/detail/func_common.inl \
+ /usr/local/include/glm/vector_relational.hpp \
+ /usr/local/include/glm/detail/func_vector_relational.inl \
+ /usr/local/include/glm/detail/compute_common.hpp \
+ /usr/local/include/glm/detail/type_vec1.hpp \
+ /usr/local/include/glm/detail/type_vec1.inl \
+ /usr/local/include/glm/ext/matrix_double3x3_precision.hpp \
+ /usr/local/include/glm/ext/matrix_float3x3.hpp \
+ /usr/local/include/glm/ext/matrix_float3x3_precision.hpp \
+ /usr/local/include/glm/mat3x4.hpp \
+ /usr/local/include/glm/ext/matrix_double3x4.hpp \
+ /usr/local/include/glm/detail/type_mat3x4.hpp \
+ /usr/local/include/glm/detail/type_mat3x4.inl \
+ /usr/local/include/glm/ext/matrix_double3x4_precision.hpp \
+ /usr/local/include/glm/ext/matrix_float3x4.hpp \
+ /usr/local/include/glm/ext/matrix_float3x4_precision.hpp \
+ /usr/local/include/glm/mat4x2.hpp \
+ /usr/local/include/glm/ext/matrix_double4x2.hpp \
+ /usr/local/include/glm/detail/type_mat4x2.hpp \
+ /usr/local/include/glm/detail/type_mat4x2.inl \
+ /usr/local/include/glm/ext/matrix_double4x2_precision.hpp \
+ /usr/local/include/glm/ext/matrix_float4x2.hpp \
+ /usr/local/include/glm/ext/matrix_float4x2_precision.hpp \
+ /usr/local/include/glm/mat4x3.hpp \
+ /usr/local/include/glm/ext/matrix_double4x3.hpp \
+ /usr/local/include/glm/detail/type_mat4x3.hpp \
+ /usr/local/include/glm/detail/type_mat4x3.inl \
+ /usr/local/include/glm/ext/matrix_double4x3_precision.hpp \
+ /usr/local/include/glm/ext/matrix_float4x3.hpp \
+ /usr/local/include/glm/ext/matrix_float4x3_precision.hpp \
+ /usr/local/include/glm/mat4x4.hpp \
+ /usr/local/include/glm/ext/matrix_double4x4.hpp \
+ /usr/local/include/glm/detail/type_mat4x4.hpp \
+ /usr/local/include/glm/detail/type_mat4x4.inl \
+ /usr/local/include/glm/geometric.hpp \
+ /usr/local/include/glm/detail/func_geometric.inl \
+ /usr/local/include/glm/exponential.hpp \
+ /usr/local/include/glm/detail/func_exponential.inl \
+ /usr/local/include/glm/ext/matrix_double4x4_precision.hpp \
+ /usr/local/include/glm/ext/matrix_float4x4.hpp \
+ /usr/local/include/glm/ext/matrix_float4x4_precision.hpp \
+ /usr/local/include/glm/detail/func_matrix.inl \
+ /usr/local/include/glm/ext/matrix_double2x2_precision.hpp \
+ /usr/local/include/glm/ext/matrix_float2x2.hpp \
+ /usr/local/include/glm/ext/matrix_float2x2_precision.hpp \
+ /usr/local/include/glm/trigonometric.hpp \
+ /usr/local/include/glm/detail/func_trigonometric.inl \
+ /usr/local/include/glm/packing.hpp \
+ /usr/local/include/glm/detail/func_packing.inl \
+ /usr/local/include/glm/detail/type_half.hpp \
+ /usr/local/include/glm/detail/type_half.inl \
+ /usr/local/include/glm/integer.hpp \
+ /usr/local/include/glm/detail/func_integer.inl \
+ /home/filthypotato/Documents/Projects/sdl3-demo/sdl3-demo/animation.h \
+ /home/filthypotato/Documents/Projects/sdl3-demo/sdl3-demo/timer.h
